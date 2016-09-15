30ba62e1c36a04d4b944b477e9f8fde0a29f72a0
Sam Edwards <uk_sam2003@hotmail.com>
2016-09-15 10:17:09 +0100

    Added .Include() to Index Action in Customer to display MembershipTypeId on the UI

--- Vidly/Controllers/CustomerController.cs
+++ Vidly/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Data.Entity;
+using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
 
@@ -21,7 +22,7 @@ namespace Vidly.Controllers
         // GET: Customer
         public ActionResult Index()
         {
-            var customers = _context.Customers.ToList();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
