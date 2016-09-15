69326266238cd9dba40df34ff9e29c6f5f04aa1d
Sam Edwards <uk_sam2003@hotmail.com>
2016-09-15 11:50:25 +0100

    Added Birthdate to Customer table via migration

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
@@ -11,5 +12,6 @@ namespace Vidly.Models
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
+        public DateTime Birthdate { get; set; }
     }
 }
